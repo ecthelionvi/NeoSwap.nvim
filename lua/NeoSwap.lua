995d053a70bd4309fb294d610da214a0ab6dd317
Robert Sears <rob@MacBook-Air-10.home>
2023-03-28 13:00:07 -0500

    Initial commit

---
+++ lua/NeoSwap.lua
@@ -0,0 +1,144 @@
+--[[
+
+
+ /$$   /$$                      /$$$$$$
+| $$$ | $$                     /$$__  $$
+| $$$$| $$  /$$$$$$   /$$$$$$ | $$  \__/ /$$  /$$  /$$  /$$$$$$   /$$$$$$
+| $$ $$ $$ /$$__  $$ /$$__  $$|  $$$$$$ | $$ | $$ | $$ |____  $$ /$$__  $$
+| $$  $$$$| $$$$$$$$| $$  \ $$ \____  $$| $$ | $$ | $$  /$$$$$$$| $$  \ $$
+| $$\  $$$| $$_____/| $$  | $$ /$$  \ $$| $$ | $$ | $$ /$$__  $$| $$  | $$
+| $$ \  $$|  $$$$$$$|  $$$$$$/|  $$$$$$/|  $$$$$/$$$$/|  $$$$$$$| $$$$$$$/
+|__/  \__/ \_______/ \______/  \______/  \_____/\___/  \_______/| $$____/
+                                                                | $$
+                                                                | $$
+                                                                |__/
+--]]
+local NeoSwap = {}
+
+local fn = vim.fn
+local api = vim.api
+local user_cmd = vim.api.nvim_create_user_command
+
+NeoSwap.setup = function()
+  -- NeoSwapNext
+  user_cmd("NeoSwapNext", "lua require('NeoSwap').swap_next(cursor_pos, type)", {})
+
+  -- NeoSwapPrev
+  user_cmd("NeoSwapPrev", "lua require('NeoSwap').swap_prev(cursor_pos, type)", {})
+end
+
+-- Swap
+local entity_pattern = {}
+entity_pattern.w = {}
+entity_pattern.w._in = "\\w"
+entity_pattern.w.out = "\\W"
+entity_pattern.w.prev_end = "\\zs\\w\\W\\+$"
+entity_pattern.k = {}
+entity_pattern.k._in = "\\k"
+entity_pattern.k.out = "\\k\\@!"
+entity_pattern.k.prev_end = "\\k\\(\\k\\@!.\\)\\+$"
+
+function NeoSwap.swap_next(cursor_pos, type)
+  type = type or "w"
+  cursor_pos = cursor_pos or "follow"
+
+  local line = api.nvim_get_current_line()
+  local cursor = api.nvim_win_get_cursor(0)
+  local c = cursor[2]
+  local line_before_cursor = line:sub(1, c + 1)
+
+  local _in = entity_pattern[type]._in
+  local out = entity_pattern[type].out
+
+  local current_word_start = fn.match(line_before_cursor, _in .. "\\+$")
+  local current_word_end = fn.match(line, _in .. out, current_word_start)
+  if current_word_end == -1 then
+    NeoSwap.swap_prev()
+    return
+  end
+
+  local next_word_start = fn.match(line, _in, current_word_end + 1)
+  if next_word_start == -1 then
+    NeoSwap.swap_prev()
+    return
+  end
+  local next_word_end = fn.match(line, _in .. out, next_word_start)
+  next_word_end = next_word_end == -1 and #line - 1 or next_word_end
+
+  local current_word = line:sub(current_word_start + 1, current_word_end + 1)
+  local next_word = line:sub(next_word_start + 1, next_word_end + 1)
+
+  local new_line = (current_word_start > 0 and line:sub(1, current_word_start) or "")
+      .. next_word
+      .. line:sub(current_word_end + 2, next_word_start)
+      .. current_word
+      .. line:sub(next_word_end + 2)
+
+  local new_c = c
+  if cursor_pos == "keep" then
+    new_c = c + 1
+  elseif cursor_pos == "follow" then
+    new_c = c + next_word:len() + next_word_start - current_word_end
+  elseif cursor_pos == "left" then
+    new_c = current_word_start + 1
+  elseif cursor_pos == "follow" then
+    new_c = c + next_word:len() + next_word_start - current_word_end + current_word_start
+  end
+
+  api.nvim_set_current_line(new_line)
+  api.nvim_win_set_cursor(0, { cursor[1], new_c - 1 })
+end
+
+function NeoSwap.swap_prev(cursor_pos, type)
+  type = type or "w"
+  cursor_pos = cursor_pos or "follow"
+
+  local line = api.nvim_get_current_line()
+  local cursor = api.nvim_win_get_cursor(0)
+  local c = cursor[2]
+  local line_before_cursor = line:sub(1, c + 1)
+
+  local _in = entity_pattern[type]._in
+  local out = entity_pattern[type].out
+  local prev_end = entity_pattern[type].prev_end
+
+  local current_word_start = fn.match(line_before_cursor, _in .. "\\+$")
+  if current_word_start == -1 then
+    NeoSwap.swap_next()
+    return
+  end
+  local current_word_end = fn.match(line, _in .. out, current_word_start)
+  current_word_end = current_word_end == -1 and #line - 1 or current_word_end
+
+  local prev_word_end = fn.match(line:sub(1, current_word_start), prev_end)
+  if prev_word_end == -1 then
+    NeoSwap.swap_next()
+    return
+  end
+  local prev_word_start = fn.match(line:sub(1, prev_word_end + 1), _in .. "\\+$")
+
+  local current_word = line:sub(current_word_start + 1, current_word_end + 1)
+  local prev_word = line:sub(prev_word_start + 1, prev_word_end + 1)
+
+  local new_line = (prev_word_start > 0 and line:sub(1, prev_word_start) or "")
+      .. current_word
+      .. line:sub(prev_word_end + 2, current_word_start)
+      .. prev_word
+      .. line:sub(current_word_end + 2)
+
+  local new_c = c
+  if cursor_pos == "keep" then
+    new_c = c + 1
+  elseif cursor_pos == "follow" then
+    new_c = c + prev_word_start - current_word_start + 1
+  elseif cursor_pos == "left" then
+    new_c = prev_word_start + 1
+  elseif cursor_pos == "follow" then
+    new_c = current_word:len() + current_word_start - prev_word_end + prev_word_start
+  end
+
+  api.nvim_set_current_line(new_line)
+  api.nvim_win_set_cursor(0, { cursor[1], new_c - 1 })
+end
+
+return NeoSwap
